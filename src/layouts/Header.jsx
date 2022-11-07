0be5f57bd7200d8c03c0496a1648886b91c7e161
MRwebdesign <mrwebdesign7@gmail.com>
2022-11-07 04:51:53 -0300

    MT-F1-03 Home 1

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/Home1/NavBar'
+import '../index.css'
+
+export default function Header() {
+  return (
+    <header className='HeaderBackG'>
+        <NavBar/>
+    </header>
+  )
+}
+
+
