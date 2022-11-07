2a8d12b76a3e5ff4b1cb418abc16c311cb97f341
MRwebdesign <mrwebdesign7@gmail.com>
2022-11-07 06:07:00 -0300

    mt-f1-05 Home1y2

---
+++ src/layouts/Home1y2.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import CallToAction from "../components/Home1/CallToAction";
+import Carrousel from "../components/Home2/Carrousel";
+import { Link } from "react-router-dom";
+import LogoImg from '../Image/LogoTimeToTravel.png'
+
+export default function Home1y2() {
+  return (
+    <div className="logoCall">
+      <div>
+        <img className="logo" src={LogoImg} alt="Logo" />
+        <div className="callToAc">
+          <Link to={"/cities"}>
+            <CallToAction title="Hotels" />
+          </Link>
+          <Link to={"/hotels"}>
+            <CallToAction title="Cities" />
+          </Link>
+        </div>
+      </div>
+      <div>
+        <Carrousel />
+      </div>
+    </div>
+  );
+}
